4fde8be91c67d92db93a3e32d70d3cccafc885d5
Michael Kamerath <mkamerath@gmail.com>
2024-09-15 17:41:56 -0600

    Adding majority error and gini index

--- DecisionTree/ID3.py
+++ DecisionTree/ID3.py
@@ -58,16 +58,28 @@ df.columns = all_attributes
 
 
 def ID3(S, attributes, label):
-    info_gain = get_information_gain(S, attributes, "info_gain")
+    split_attribute = get_information_gain(S, attributes, "gini_index")
+    print(split_attribute)
+    return 0
 
 
 def get_information_gain(examples, attributes, method):
     total_values = len(examples.index)
     all_label_counts = examples.groupby("label").size().to_list()
-    print(all_label_counts)
-    starting_entropy = sum(
-        [-x / total_values * math.log2(x / total_values) for x in all_label_counts]
-    )
+    best_split = ""
+    best_info_gain = 0
+    if method == "info_gain":
+        starting_entropy = sum(
+            [-x / total_values * math.log2(x / total_values) for x in all_label_counts]
+        )
+    elif method == "major_err":
+        starting_entropy = (sum(all_label_counts) - max(all_label_counts)) / sum(
+            all_label_counts
+        )
+    elif method == "gini_index":
+        starting_entropy = 1 - sum(
+            [(x / sum(all_label_counts)) ** 2 for x in all_label_counts]
+        )
     print(starting_entropy)
     for attribute in set(all_attributes) - set(attributes) - set(["label"]):
         vals = examples[attribute].unique()
@@ -79,28 +91,31 @@ def get_information_gain(examples, attributes, method):
                 .size()
                 .to_list()
             )
-            print(thing)
             summation = sum(thing)
-            print(summation)
-            print(total_values)
-            attribute_info_gain.append(
-                sum([-x / summation * math.log2(x / summation) for x in thing])
-                * summation
-                / total_values
-            )
-        print(sum(attribute_info_gain))
-        print(starting_entropy - sum(attribute_info_gain))
-
-    # counts = examples.groupby("label").count()
-    # print(counts)
-    # exit()
-    # counts = [x for x in examples.groupby(["label"]).count().iloc[:, 0]]
-    # print(counts)
-    # entropy = [-x / total_values * math.log2(x / total_values) for x in counts]
-    # print(entropy)
-    # exit()
-    # counts = examples.groupby(["buying", "label"]).size().reset_index(name="counts")
-    # print(counts)
+            if method == "info_gain":
+                attribute_info_gain.append(
+                    sum([-x / summation * math.log2(x / summation) for x in thing])
+                    * summation
+                    / total_values
+                )
+            elif method == "major_err":
+                attribute_info_gain.append(
+                    (summation - max(thing)) / float(total_values)
+                )
+            elif method == "gini_index":
+                attribute_info_gain.append(
+                    (1 - sum([(x / summation) ** 2 for x in thing]))
+                    * summation
+                    / total_values
+                )
+        # Uncomment the line below to check what the information gain is of each attribute
+        print(
+            f"Attribute: {attribute}\nInformation gain: {starting_entropy - sum(attribute_info_gain)}"
+        )
+        if best_info_gain <= starting_entropy - sum(attribute_info_gain):
+            best_info_gain = starting_entropy - sum(attribute_info_gain)
+            best_split = attribute
+    return best_split
 
 
 ID3(df, [], "unacc")
