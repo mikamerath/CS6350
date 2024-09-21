0f5449e90cb927b772c95f89f9db5c6847949695
Michael Kamerath <mkamerath@gmail.com>
2024-09-20 18:17:15 -0600

    Everything is working

--- DecisionTree/ID3.py
+++ DecisionTree/ID3.py
@@ -1,32 +1,63 @@
-import pandas as pd
 import math
 
+from pandas.api.types import is_numeric_dtype
+
+import numpy as np
+
 attributes_label = ""
-max_depth = 6
-metric = "info_gain"
+max_depth = 0
+metric = ""
 
 
 class ID3Tree:
     def __init__(self, data):
         self.children = []
+        self.attribute = None
         self.data = data
 
 
+def ID3_prepare(S, attributes, label, depth):
+    S_result = S.copy(deep=True)
+    for attribute in attributes:
+        if is_numeric_dtype(S_result[attribute]):
+            median = S_result[attribute].median()
+            S_result[attribute] = S_result[attribute].apply(
+                lambda x: 1 if x >= median else 0
+            )
+
+    # Comment out for using unknown as a value
+    for attribute in attributes:
+        mode = S_result[attribute].mode()
+        # print(mode)
+        if mode[0] == "unknown":
+            mode = S_result[attribute].value_counts().index.tolist()[1]
+        if mode[0] == "f":
+            S_result[attribute] = S_result[attribute].apply(
+                lambda x: mode if x == "unknown" else x
+            )
+        else:
+            S_result[attribute] = S_result[attribute].apply(
+                lambda x: mode[0] if x == "unknown" else x
+            )
+
+    return ID3(S_result, attributes, label, depth)
+
+
 def ID3(S, attributes, label, depth):
-    if len(set(S[attributes_label])) == 1:
+    if len(list(S[attributes_label].unique())) == 1:
         return ID3Tree(S[attributes_label].iloc[0])
-    if len(attributes) == 0 or depth == max_depth:
+    if len(attributes) == 0 or depth >= max_depth:
         return ID3Tree(S[attributes_label].mode().iloc[0])
 
     root = ID3Tree(label)
     # Last arguments should be info_gain, major_err, or gini_index
     split_attribute = get_information_gain(S, attributes, metric)
+    root.attribute = split_attribute
     vals = S[split_attribute].unique()
-    print(split_attribute)
     for val in vals:
         subset_examples = S[S[split_attribute] == val]
-        if len(subset_examples) == 0:
-            root.data = ID3Tree(S[attributes_label].mode().iloc[0])
+        if len(subset_examples.index) == 0:
+            root.children.append(ID3Tree(S[attributes_label].mode().iloc[0]))
         else:
             root.children.append(
                 ID3(subset_examples, attributes - {split_attribute}, val, depth + 1)
@@ -51,7 +82,6 @@ def get_information_gain(examples, attributes, method):
         starting_entropy = 1 - sum(
             [(x / sum(all_label_counts)) ** 2 for x in all_label_counts]
         )
-    print(starting_entropy)
     for attribute in set(attributes):
         vals = examples[attribute].unique()
         attribute_info_gain = []
@@ -62,28 +92,40 @@ def get_information_gain(examples, attributes, method):
                 .size()
                 .to_list()
             )
-            summation = sum(thing)
-            if method == "info_gain":
-                attribute_info_gain.append(
-                    sum([-x / summation * math.log2(x / summation) for x in thing])
-                    * summation
-                    / total_values
-                )
-            elif method == "major_err":
-                attribute_info_gain.append(
-                    (summation - max(thing)) / float(total_values)
-                )
-            elif method == "gini_index":
-                attribute_info_gain.append(
-                    (1 - sum([(x / summation) ** 2 for x in thing]))
-                    * summation
-                    / total_values
-                )
+            get_info_gain_of_examples(thing, method, total_values, attribute_info_gain)
+
         # Uncomment the line below to check what the information gain is of each attribute
-        print(
-            f"Attribute: {attribute}\nInformation gain: {starting_entropy - sum(attribute_info_gain)}"
-        )
+        # print(
+        #     f"Attribute: {attribute}\nInformation gain: {starting_entropy - sum(attribute_info_gain)}"
+        # )
         if best_info_gain <= starting_entropy - sum(attribute_info_gain):
             best_info_gain = starting_entropy - sum(attribute_info_gain)
             best_split = attribute
+    # print(f"Best split {best_split}, information gain {best_info_gain}")
     return best_split
+
+
+def get_info_gain_of_examples(thing, method, total_values, attribute_info_gain):
+    summation = sum(thing)
+    if method == "info_gain":
+        attribute_info_gain.append(
+            sum([-x / summation * math.log2(x / summation) for x in thing])
+            * summation
+            / total_values
+        )
+    elif method == "major_err":
+        attribute_info_gain.append((summation - max(thing)) / float(total_values))
+    elif method == "gini_index":
+        attribute_info_gain.append(
+            (1 - sum([(x / summation) ** 2 for x in thing])) * summation / total_values
+        )
+
+
+def print_tree(tree):
+    queue = []
+    queue.append(tree)
+    while len(queue) > 0:
+        node = queue.pop(0)
+        print(f"{node.data}, {node.attribute}    ")
+        for child in node.children:
+            queue.append(child)
--- DecisionTree/test_ID3.py
+++ DecisionTree/test_ID3.py
@@ -1,85 +1,228 @@
 import pandas as pd
+
+from pandas.api.types import is_numeric_dtype
+
 import ID3
 
-# Set this to which data you want to use (car or bank)
-choice = "car"
+data_path = "car/"
+all_attributes = [
+    "buying",
+    "maint",
+    "doors",
+    "persons",
+    "lug_boot",
+    "safety",
+    "label",
+]
+attributes_label = "label"
 
-# Set this to be the max depth allowed by the tree
-max_depth = 1
+bank_data_path = "bank/"
+bank_all_attributes = [
+    "age",
+    "job",
+    "marital",
+    "education",
+    "default",
+    "balance",
+    "housing",
+    "loan",
+    "contact",
+    "day",
+    "month",
+    "duration",
+    "campaign",
+    "pdays",
+    "previous",
+    "poutcome",
+    "y",
+]
 
-if choice == "car":
-    data_path = "car/"
-    all_attributes = [
-        "buying",
-        "maint",
-        "doors",
-        "persons",
-        "lug_boot",
-        "safety",
-        "label",
-    ]
-    attributes_label = "label"
-else:
-    data_path = "bank/"
-    all_attributes = [
-        "age",
-        "job",
-        "marital",
-        "education",
-        "default",
-        "balance",
-        "housing",
-        "loan",
-        "contact",
-        "day",
-        "month",
-        "duration",
-        "campaign",
-        "pdays",
-        "previous",
-        "poutcome",
-        "y",
-    ]
-    attributes_label = "y"
+bank_numerical_attriubues = [
+    "age",
+    "balance",
+    "day",
+    "duration",
+    "campaign",
+    "pdays",
+    "previous",
+]
 
+bank_attributes_label = "y"
+
+attributes_map = {k: v for v, k in enumerate(all_attributes)}
 test_data_path = data_path + "test.csv"
 data_path += "train.csv"
 
-df = pd.read_csv(data_path)
-df.columns = all_attributes
+bank_attributes_map = {k: v for v, k in enumerate(bank_all_attributes)}
+bank_test_data_path = bank_data_path + "test.csv"
+bank_data_path += "train.csv"
 
-ID3.attributes_label = attributes_label
-ID3.max_depth = max_depth
-ID3.metric = "info_gain"
 
 six_trees = []
+sixteen_trees = []
 
-for i in range(1, 7):
-    six_trees.append(ID3.ID3(df, set(all_attributes[:-1]), "", i))
+car_df = pd.read_csv(data_path, names=all_attributes)
+car_df.columns = all_attributes
+bank_df = pd.read_csv(bank_data_path, names=bank_all_attributes)
+bank_df.columns = bank_all_attributes
 
-print(len(six_trees))
+# for i in range(1, 7):
+#     df = pd.read_csv(data_path, names=all_attributes)
+#     ID3.attributes_label = attributes_label
+#     ID3.max_depth = i
+#     ID3.metric = "major_err"
+#     six_trees.append(ID3.ID3_prepare(df, set(all_attributes[:-1]), "root", 0))
+
+for i in range(1, 17):
+    df = pd.read_csv(bank_data_path, names=bank_all_attributes)
+    ID3.attributes_label = bank_attributes_label
+    ID3.max_depth = i
+    ID3.metric = "major_err"
+    sixteen_trees.append(ID3.ID3_prepare(df, set(bank_all_attributes[:-1]), "root", 0))
+
+# print(len(six_trees))
+# print(len(sixteen_trees))
 
 
 # Returns true is the value in example matches
-def test_observation(tree, test_example, possible_outputs):
-    if tree.data in possible_outputs:
+def test_observation(tree, test_example, possible_outputs, a_map):
+    if tree.data in possible_outputs and len(tree.children) == 0:
         if test_example[-1] == tree.data:
             return True
-
+        return False
     for child in tree.children:
-        return test_observation(child, test_example, possible_outputs)
-    return False
+        if (
+            test_example[a_map[tree.attribute]] == child.data
+            or child.data in possible_outputs
+        ):
+            return test_observation(child, test_example, possible_outputs, a_map)
 
 
-def test_tree(tree, examples_path):
+# Tests the resulting ID3 tree for training or test examples
+def test_tree(tree, examples_path, labels, a_map, a_df, a_mode_map):
     result = []
     with open(examples_path, "r") as file:
         for line in file:
             example = line.strip().split(",")
-            result.append(
-                test_observation(tree, example, ["unacc", "acc", "good", "vgood"])
-            )
+            if "bank" in examples_path:
+                example = convert_example_with_medians(example, a_df)
+                example = convert_example_with_mode(example, a_mode_map)
+            result.append(test_observation(tree, example, labels, a_map))
     return result.count(True) / len(result)
 
 
-print(test_tree(six_trees[0], data_path))
+# Converts numerical parts of an example to binary
+def convert_example_with_medians(example, b_df):
+    for i, _ in enumerate(example):
+        if bank_all_attributes[i] in bank_numerical_attriubues:
+            med = b_df[bank_all_attributes[i]].median()
+            example[i] = 1 if float(example[i]) >= med else 0
+
+    return example
+
+
+def convert_example_with_mode(example, att_modes):
+    for i, _ in enumerate(example):
+        if example[i] == "unknown":
+            example[i] = att_modes[bank_all_attributes[i]]
+
+    return example
+
+
+def convert_df_unknown(c_df, attributes):
+    for attribute in attributes:
+        if is_numeric_dtype(c_df[attribute]):
+            median = c_df[attribute].median()
+            c_df[attribute] = c_df[attribute].apply(lambda x: 1 if x >= median else 0)
+
+    # Comment out for using unknown as a value
+    for attribute in attributes:
+        mode = c_df[attribute].mode()
+        if mode[0] == "unknown":
+            mode = c_df[attribute].value_counts().index.tolist()[1]
+        if mode[0] == "f":
+            c_df[attribute] = c_df[attribute].apply(
+                lambda x: mode if x == "unknown" else x
+            )
+        else:
+            c_df[attribute] = c_df[attribute].apply(
+                lambda x: mode[0] if x == "unknown" else x
+            )
+
+    return c_df
+
+
+def get_mode_df_col(mode_df, attributes):
+    attributes_to_mode = {}
+    for attribute in attributes:
+        attributes_to_mode[attribute] = mode_df[attribute].mode()[0]
+    return attributes_to_mode
+
+
+# train_error = []
+# for tree in six_trees:
+#     train_error.append(
+#         test_tree(tree, data_path, ["unacc", "acc", "good", "vgood"], attributes_map)
+#     )
+
+# test_error = []
+# for tree in six_trees:
+#     test_error.append(
+#         test_tree(
+#             tree, test_data_path, ["unacc", "acc", "good", "vgood"], attributes_map
+#         )
+#     )
+
+# print(f"Training error {1 - sum(train_error) / len(train_error)}")
+# print(f"Testing error {1 - sum(test_error) / len(test_error)}")
+
+bank_df = convert_df_unknown(bank_df, bank_all_attributes)
+attributes_modes = get_mode_df_col(bank_df, bank_all_attributes[:-1])
+bank_train_error = []
+for tree in sixteen_trees:
+    bank_train_error.append(
+        test_tree(
+            tree,
+            bank_data_path,
+            ["yes", "no"],
+            bank_attributes_map,
+            bank_df,
+            attributes_modes,
+        )
+    )
+
+bank_test_error = []
+for tree in sixteen_trees:
+    bank_test_error.append(
+        test_tree(
+            tree,
+            bank_test_data_path,
+            ["yes", "no"],
+            bank_attributes_map,
+            bank_df,
+            attributes_modes,
+        )
+    )
+
+print(f"Training error {1 - sum(bank_train_error) / len(bank_train_error)}")
+print(f"Testing error {1 - sum(bank_test_error) / len(bank_test_error)}")
+
+# df = pd.read_csv(bank_data_path, names=bank_all_attributes)
+# ID3.attributes_label = bank_attributes_label
+# ID3.max_depth = 5
+# ID3.metric = "info_gain"
+# tree_result = ID3.ID3_prepare(df, set(bank_all_attributes[:-1]), "root", 0)
+
+# df = convert_df_unknown(df, bank_all_attributes[:-1])
+
+# print(
+#     test_tree(
+#         tree_result,
+#         bank_test_data_path,
+#         ["yes", "no"],
+#         bank_attributes_map,
+#         df,
+#         attributes_modes,
+#     )
+# )
+# print(ID3.print_tree(tree_result))
